e7c495ec277a73373fd0294f209b63d06501c0d7
Ali Ahmad <alislearning123@gmail.com>
2020-09-15 15:01:25 -0400

    Set up boiler plate for express application

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express"),
+  app = express(),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);

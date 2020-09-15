c7470738749a0a74d7f78a3239588af0068e9111
Ali Ahmad <alislearning123@gmail.com>
2020-09-15 15:19:55 -0400

    Add Material Design Bootstrap and link CSS and JavaScript front-end files

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.render("index");
 });
 
 let port = process.env.PORT;
